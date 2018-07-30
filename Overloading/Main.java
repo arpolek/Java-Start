44a4af6ea89866a82a0ba08492025bfdfaf7b4aa
arpolek <38766329+arpolek@users.noreply.github.com>
2018-07-30 18:59:35 +0200

    Add files via upload

---
+++ Overloading/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Test test = new Test();
+		System.out.println(test.add(5.0, 2.0));
+		System.out.println(test.add(2, 3));
+		System.out.println(test.add(1, 2, 3));
+
+	}
+
+}
---
+++ Overloading/Test.java
@@ -0,0 +1,19 @@
+
+public class Test {
+
+	public int add(int a, int b){
+		System.out.println("Suma dwoch int typu int: ");
+		return a+b;
+	}
+	
+	public  double add(double a, double b) {
+		System.out.println("Suma dwoch double typu double: ");
+		return a+b;
+	}
+	
+	public double add (int a, int b, int c) {
+		System.out.println("Suma trzech int typu double: ");
+		return a+b+c;
+	}
+	
+}

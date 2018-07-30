4c35291e1f3f9d66835cffba3801e2777ad4fce8
arpolek <38766329+arpolek@users.noreply.github.com>
2018-07-30 19:00:54 +0200

    Add files via upload

---
+++ Punkty/Punkt.java
@@ -0,0 +1,21 @@
+
+public class Punkt {
+	int wspX, wspY;
+	
+	void setX(int x) {
+		wspX = x;
+	}
+	
+	void setY(int y) {
+		wspY = y;
+	}
+	
+	int getX() {
+		return wspX;
+	}
+	
+	int getY() {
+		return wspY;
+	}
+}
+
---
+++ Punkty/UstawPunkt.java
@@ -0,0 +1,12 @@
+
+public class UstawPunkt {
+
+	public static void main(String[] args) {
+		Punkt punkt1 = new Punkt();
+		punkt1.setX(10);
+		punkt1.setY(20);
+		System.out.println("Wspolrzedne punktu to ("+punkt1.wspX+","+punkt1.wspY+")");
+
+	}
+
+}
